243e7492fb1a572a9dc789cdac515d156d5c5db3
Xander3359 <66163761+Xander3359@users.noreply.github.com>
2024-03-10 08:47:18 -0400

    testing vars

--- source/main.lua
+++ source/main.lua
@@ -1,36 +1,22 @@
-import "CoreLibs/graphics"
-
-local pd <const> = playdate
-local gfx <const> = playdate.graphics
-
-local playerX, playerY = 200, 120
-local playerRadius = 10
-
-local PlayerSpeed = 3 -- Baseline speed before any modifiers
-local CurrentPlayerSpeed = PlayerSpeed
-
-function pd.update()
-	gfx.clear()
-	if(pd.buttonIsPressed(pd.kButtonUp)) then -- Up and down are inverted for some reason
-		playerY -= CurrentPlayerSpeed
-	end
-	if(pd.buttonIsPressed(playdate.kButtonDown)) then -- Up and down are inverted for some reason
-		playerY += CurrentPlayerSpeed
-	end
-	if(pd.buttonIsPressed(pd.kButtonRight)) then
-		playerX += CurrentPlayerSpeed
-	end
-	if(pd.buttonIsPressed(pd.kButtonLeft)) then
-		playerX -= CurrentPlayerSpeed
-	end
-
-	if(pd.buttonIsPressed(pd.kButtonB)) then
-		CurrentPlayerSpeed = PlayerSpeed * 2
-	end
-	if(pd.buttonIsPressed(pd.kButtonA)) then
-		CurrentPlayerSpeed = PlayerSpeed / 2
-	end
+a = 1
+b = 10
 
 
-	gfx.fillCircleAtPoint(playerX, playerY, playerRadius)
+print(a)
+print(b)
+
+if(a) then
+	print(a)
+end
+
+if(kButtonA) then
+	a = 0
+end
+
+if(!a) then
+	print("no A")
+end
+
+function playdate.update()
+	
 end
